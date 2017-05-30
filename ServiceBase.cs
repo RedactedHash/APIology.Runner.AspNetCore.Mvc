16f635ade8bda582ec4f90c0e1f22affd9531ee9
Isaac A. Sanch <koruptid@gmail.com>
2017-05-30 09:54:13 -0400

    Add existing development progress.

---
+++ ServiceBase.cs
@@ -0,0 +1,33 @@
+﻿namespace APIology.Runner.AspNetCore.Mvc
+{
+	using System.Diagnostics.CodeAnalysis;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.DependencyInjection;
+
+	[SuppressMessage("ReSharper", "MemberCanBeProtected.Global")]
+	public abstract class ServiceBase<TAPIBase, TConfiguration> : AspNetCore.ServiceBase<TAPIBase, TConfiguration>
+		where TAPIBase : ServiceBase<TAPIBase, TConfiguration>
+		where TConfiguration : Configuration, new()
+	{
+		public override void ConfigureAspNetCore(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
+
+		public override void BuildAspNetCoreApp(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+			}
+
+			app.UseStaticFiles();
+		}
+	}
+}
